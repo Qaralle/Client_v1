e6594bbafc0ea45896e7597ba63222be323a05b7
Qaralle <57999732+Qaralle@users.noreply.github.com>
2020-04-28 21:08:11 +0300

    lol its me

---
+++ src/ClientMain.java
@@ -0,0 +1,16 @@
+public class ClientMain
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Validator3000 terminal = new Validator3000();
+        terminal.interactiveMod("$");
+
+/*        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));*/
+
+        String str = null;
+        String str2 = str;
+
+    }
+}
